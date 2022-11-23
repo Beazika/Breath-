4066a60460eb034982d0fff06dd601e76d70c0d9
Bee <93218052+Beazika@users.noreply.github.com>
2022-11-23 10:27:40 -0300

    uptade de cria

--- Breath-main/Breath/script.js
+++ Breath-main/Breath/script.js
@@ -1,22 +1,8 @@
-$(window).scroll(function() {
-    // 100 = The point you would like to fade the nav in.
-      
-        if ($(window).scrollTop() > 100 ){
-        
-             $('.bg').addClass('show');
-        
-      } else {
-        
-        $('.bg').removeClass('show');
-        
-         };   	
-    });
-    
-    $('.scroll').on('click', function(e){		
-            e.preventDefault()
-        
-      $('html, body').animate({
-          scrollTop : $(this.hash).offset().top
-        }, 1500);
-    });
+
+    function leiaMais(){
+      document.getElementById('leiamais').style.display='flex';
+    }
+    function leiaMenos(){
+      document.getElementById('leiamais').style.display='none';
+    }
 
